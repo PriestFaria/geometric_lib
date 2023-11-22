1c2f8b4305b572501e2064cf4f49fa36447fb6a3
Timur Valeev <76071256+PriestFaria@users.noreply.github.com>
2023-11-22 13:51:53 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -7,15 +7,4 @@ def perimeter(a, b, c):
     """Принимает значения a, b и c, возвращает периметр треугольника с данными сторонами"""
     return a + b + c 
 
-class TriangleTestCase(unittest.TestCase):
-    def test_zero_mul(self):
-        res = area(100, 0)
-        self.assertEqual(res, 0)
 
-    def test_area_mul(self):
-        res = area(10, 5)
-        self.assertEqual(res, 25)
-
-    def test_perimeter(self):
-        res = perimeter(10,15,20)
-        self.assertEqual(res, 45)
