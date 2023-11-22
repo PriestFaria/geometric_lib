9128d2fb1bd026cbb3d81439482690cf72d68075
Timur Valeev <76071256+PriestFaria@users.noreply.github.com>
2023-11-22 13:50:42 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -11,14 +11,4 @@ def perimeter(r):
     """Принимает число r, возвращает значение длины окружности радиусом r"""
     return 2 * math.pi * r
 
-class CircleTestCase(unittest.TestCase):
-    def test_zero_mul(self):
-        res1 = area(0)
-        res2 = perimeter(0)
-        self.assertEqual(res1, 0)
-        self.assertEqual(res2, 0)
-    def test_mul(self):
-        res1 = area(10)
-        res2 = perimeter(10)
-        self.assertEqual(res1, 314.1592653589793)
-        self.assertEqual(res2, 62.83185307179586)
+
