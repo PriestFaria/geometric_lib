47d3092f5f1da486e1394b7b8886e35f4f038125
Timur Valeev <76071256+PriestFaria@users.noreply.github.com>
2023-11-22 13:51:34 +0300

    Update square.py

--- square.py
+++ square.py
@@ -8,15 +8,3 @@ def perimeter(a):
     """Принимает значение a, возвращает значение периметра квадрата со стороной a"""
     return 4 * a
 
-class SquareTestCase(unittest.TestCase):
-    def test_nul_mul(self):
-        res1 = area(0)
-        res2 = perimeter(0)
-        self.assertEqual(res1, 0)
-        self.assertEqual(res2, 0)
-
-    def test_mul(self):
-        res1 = area(10)
-        res2 = perimeter(10)
-        self.assertEqual(res1, 100)
-        self.assertEqual(res2, 40)
