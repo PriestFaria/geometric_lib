d4330f91411f4c760a1f0d73f8fc380cb1f28259
Timur Valeev <76071256+PriestFaria@users.noreply.github.com>
2023-11-22 13:51:12 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -8,11 +8,3 @@ def perimeter(a, b):
     """Принимает значения a и b, возвращает значение периметра прямоугольника с данными сторонами"""
     return (a + b)*2 
 
-class RectangleTestCase(unittest.TestCase):
-    def test_zero_mul(self):
-        res = area(10,0)
-        self.assertEqual(res, 0)
-        
-    def test_square_mul(self):
-        res = area(10,10)
-        self.assertEqual(res, 100)
